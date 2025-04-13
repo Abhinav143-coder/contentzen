86644b254b11edbcc5e4e47b87fbf55e89ceb82e
Abhinav143-coder <abhinav782005@gmail.com>
2025-04-13 13:27:49 +0530

    Create Index2 .jsx

---
+++ Index2 .jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContentZenPage = () => {
+  return (
+    <div>
+      <h1>Welcome to ContentZen</h1>
+      <p>This is a simple AI-powered topic idea generator landing page.</p>
+    </div>
+  );
+};
+
+export default ContentZenPage;
